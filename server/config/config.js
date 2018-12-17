e78e4e50cd9c8f9f49dc67a429aa57f399efc941
NGM-PC\Neider Galofre <neider.galofre@zabud.co>
2018-12-17 11:02:45 -0500

    correccion de variable de coneccion a mongo en la nube

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
+if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    // MONGO_URI está creada en las credenciales del proyecto en heroku
-    urlDB = MONGO_URI;
+    // MONGO_URI está creada en el entorno del proyecto e heroku 
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
